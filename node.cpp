f349572ae13ee0ef59a3d455fa2be87e3b464c49
Aposdin <pistaciolover@hotmail.com>
2018-12-05 13:52:28 -0800

    Add files via upload

--- node.cpp
+++ node.cpp
@@ -1,14 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// CPE 400 Project
+// dynamic routing mechanism design with focus on throughput
+//
+// Edited by
+// David Neilson: Node class
+// Nicholas Rini: edited Node class, created Network
+// Michael Mills: edited node class, added linkStateAlgorithm, added file stream, added packet class
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <ctime>
 #include <vector>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
 class Node{
 
-public:
+	public:
+	
     int ID;
 	int queue;
 	int capacity;
 	int pSpeed;
-	int weight = 1;
+	int weight;
 	// forwarding table
 	vector<int> destination;
 	vector<int> link;
@@ -26,6 +43,7 @@ Node::Node(){
 	queue = 0;
 	capacity = 5;
 	pSpeed = 5;
+	weight = 1;
 }
 
 Node::~Node(){
@@ -36,7 +54,7 @@ Node::~Node(){
 
 // make forwarding table through link state algorithm, this means the network is centralized ospf
 // the algorithm is dijkstra's page 393 in the book.
-void linkStateAlgorithm( Node* array, int arrayNum )
+void linkStateAlgorithm( Node* array, int arrayNum ) // aka dijkstra's
 {
 	// initialize
 	arrayNum;
@@ -44,15 +62,15 @@ void linkStateAlgorithm( Node* array, int arrayNum )
 	int nsize,i,j;
 	for( i = 0; i < arrayNum; i++)
 	{
-		for( j = 0; j < arrayNum-1; j++ )
+		for( j = 0; j < arrayNum-1; j++ )			// the initialize
 		{
 			cost[i] = 0; // 0 will represent infinity
 		}
 		
-		nsize = array[i].neighbors.size()
+		nsize = array[i].neighbors.size()			// the loop
 		for( int j = 0; j < nsize; j++)
 		{
-			cost[array[i].neighbors[nsize].ID] = array[i].neighbors[nsize].weight;
+			cost[array[i].neighbors[j].ID] = array[i].neighbors[j].weight;
 		}
 		for( int j = 0; j < arrayNum; j++ )
 		{
@@ -61,16 +79,60 @@ void linkStateAlgorithm( Node* array, int arrayNum )
 	}		
 }
 
-int main(){
+// packet class
+class Packet
+{
+	public:
+	// udp style packet leaving out length and checksum
+	int source;
+	int destination;
 
+	// data in packet, not creating separate data structure
+	int ID;	
+	time_t startTime;
+	time_t endTime;
+	int hops;
+	vector<int> nodeStop;
+	vector<int> queueAtNode;
+	
+	makePacket( int s, int d, int id );
+	
+}
+Packet::makePacket( int s, int d, int id);
+{
+	source = s;
+	destination = d;
+	ID = id;
+	startTime = time(0);
+	hops = 0;
+	nodeStop.push_back( s );
+	queueAtNode.push_back( id );
+}
+
+int main( int argc, char* argv[] )
+{
+	int i, j, k = 0, numPackets = 0;
+	int rsource[argc/3], rdestination[argc/3], rpacketnum[argc/3];
+	// read in from command line the source destinatin and # of packets
+	for( i = 0; i < argc/3; i++ )
+	{
+		rsource[i] = atoi( argv[k] );
+		k++;
+		rdestination[i] = atoi( argv[k] );
+		k++;
+		rpacketnum[i] = atoi( argv[k] );
+		k++;
+	}
+	
+	// create the network
 	Node* array = new Node[8];
 
-	for( int i = 0; i < 8; i++)
+	for( i = 0; i < 8; i++)
 	{
 		array[i].ID = i;
 
 	}
-
+	// creates network
 	array[0].neighbors.push_back(array[1]);
 	array[0].neighbors.push_back(array[4]);
 
@@ -96,8 +158,43 @@ int main(){
 
 	array[8].neighbors.push_back(array[5]);
 	array[8].neighbors.push_back(array[6]);
+	
+	// create packets for the network
+	for( i = 0; i < argc/3; i++ )
+	{
+		numPackets += rpacketnum[i];
+	}
+	Packet rpacket[numPackets]
+	k = 0;
+	for( i = 0; i < argc/3; i++ )
+	{
+		for( j = 0; j < rpacketnum[i]; j++ )
+		{
+			rpackets[k] = makePacket( rsource[i], rdestination[i], k )
+		}
+	}
+	
+	// loop through packets and nodes
+	
+	// move packets through network one node at a time
 
 
+	// file output
+	ofstream results ("results.txt");
+	if( results.is_open())
+	{
+		// calculate overall throughput and output to file // average( endTime - startTime + ( hops * nodalDelay ) )
+		
+		// output #of packets lost
+		
+		// output data from packets
+		
+		
+		
+		// close file
+		results.close();
+	}
+	
 	return 0;
 
 }
